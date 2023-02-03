8dfce55b427988558154c5486174625dc5f82746
Abdallak2202 <96998579+Abdallak2202@users.noreply.github.com>
2023-02-02 22:23:29 -0500

    Redefined models, and their relationship

--- api/src/models/Order.js
+++ api/src/models/Order.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-  sequelize.define('orders', {
+  sequelize.define('order', {
     id: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
