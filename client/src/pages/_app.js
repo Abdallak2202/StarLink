7472bbdd69500c07f1a62286fcba8aad69c75736
L. Carolina Arias <joseluy123@gmail.com>
2023-02-04 15:14:33 -0500

    refactor(client/components/nav and login) modyfy navbar and add login

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,12 +1,50 @@
-import '@/styles/globals.css'
 import NavBarMain from 'components/nav/NavBarMain'
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import {magic} from "../../lib/magic-client";
+import "../styles/globals.css";
+import Loading from 'components/loading/Loading';
 
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const handleLoggedIn = async () => {
+      const isLoggedIn = await magic.user.isLoggedIn();
+      if (isLoggedIn) {
+        // route to /
+        router.push("/");
+      } else {
+        // route to /login
+        router.push("/login");
+      }
+    };
+    handleLoggedIn();
+  }, []);
+
+  useEffect(() => {
+    const handleComplete = () => {
+      setIsLoading(false);
+    };
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  }, [router]);
   return (
-  <div>
-  <NavBarMain username="random user"/>
-  <Component {...pageProps} />
+    <>
+      {isLoading ? <Loading />
+  :<div>
+  {router.pathname !== "/login" && 
+  <NavBarMain />}
+  <Component {...pageProps} />;
   </div>
+}
+    </>
 )}
 
