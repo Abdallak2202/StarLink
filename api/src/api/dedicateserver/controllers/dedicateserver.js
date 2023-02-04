5e72fd3dc2b38acb9d944ead9138e860c1f7a63c
luicho76 <luishg.ingeniero@gmail.com>
2023-02-04 19:53:53 -0400

    Actualizacion de carpeta API

---
+++ api/src/api/dedicateserver/controllers/dedicateserver.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * dedicateserver controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::dedicateserver.dedicateserver');
---
+++ api/src/api/dedicateserver/routes/dedicateserver.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * dedicateserver router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::dedicateserver.dedicateserver');
