4ea55d42536df5c2675db8f9c92b718acb767875
silvinaarguello <silvinaaguello@gmail.com>
2023-02-05 02:27:15 -0300

    Acomodando cards ruta Dominio-com-co

--- client/components/card-dominio-cpm-co/cardDominios.jsx
+++ client/components/card-dominio-cpm-co/cardDominios.jsx
@@ -1,5 +1,6 @@
 import s from "src/styles/cardDominio.module.css"
-import Link from "next/link"
+import "tailwindcss/tailwind.css";
+
 
 export default function Card (){
   return (
@@ -9,14 +10,14 @@ export default function Card (){
       <div className= {s.flipCardFront}>
 
       <ul>
-      <h1>.Com</h1>
+      <h1 className="text-sm font-medium text-gray-900">.Com</h1>
       <h2> Por solo</h2>
      <h3> $1790</h3>       
       </ul>
       </div>
       <div className={s.flipCardBack}>
-        <h1>Utilidades del dominio .Com</h1>
-        <p>Es uno de los más utilizados a nivel mundial
+        <h1 className={s.h1}>Utilidades del dominio .Com</h1>
+        <p className={s.p}>Es uno de los más utilizados a nivel mundial
         Se le considera un dominio de nivel superior
         Es muy usado en el ambito comercial</p>
       </div>
@@ -36,10 +37,8 @@ export default function Card (){
       </ul>
       </div>
       <div className={s.flipCardBack}>
-        <h1>¿Por qué 3 dominios?</h1>
-        <p>En el último tiempo han surgido nuevas extensiones de dominios para que las personas puedan elegir
-         el nombre de dominio que desean para su marca o proyecto digital
-        Con la aparición de dominios tan creativos, las marcas comenzaron a registrarlos 
+        <h1 className={s.h1}>¿Por qué 3 dominios?</h1>
+        <p className={s.p}>Con la aparición de dominios tan creativos, las marcas comenzaron a registrarlos 
         también para proteger su marca y evitar que terceros puedan hacer uso de ellos.</p>
       </div>
       </div>
@@ -56,8 +55,8 @@ export default function Card (){
       </ul>
       </div>
       <div className={s.flipCardBack}>
-        <h1>Registrate desde cualquier lugar</h1>
-        <p>Los clientes podrán identificar claramente la localización de tu negocio</p>
+        <h1 className={s.h1}>Registrate desde cualquier lugar</h1>
+        <p className={s.p}>Los clientes podrán identificar claramente la localización de tu negocio</p>
       </div>
       </div>
       
@@ -73,10 +72,8 @@ export default function Card (){
       </ul>
       </div>
       <div className={s.flipCardBack}>
-        <h1>Promueve tu .site con claridad</h1>
-        <p> es una abreviatura de sitio web y le dice a los usuarios de
-            Internet que su marca es una marca tecnológicamente inteligente.
-            .SITE funciona como una aplicación para marketing y promoción y ayuda a su
+        <h1 className={s.h1}>Promueve tu .site con claridad</h1>
+        <p className={s.p}>Funciona como una aplicación para marketing y promoción y ayuda a su
                 marca a diferenciarse en un mar saturado de sitios web en línea.</p>
       </div>
       </div>
@@ -94,10 +91,8 @@ export default function Card (){
       </ul>
       </div>
       <div className={s.flipCardBack}>
-        <h1>El dominio más exitoso de la nueva Internet</h1>
-        <p>Finalizar el nombre de tu sitio web con una extensión 
-        novedosa como .ONLINE, puede ayudarte a destacar entre la competencia.
-        La palabra online es fácilmente entendida en todo el mundo 
+        <h1 className={s.h1}>El dominio más exitoso de la nueva Internet</h1>
+        <p className={s.p}>La palabra online es fácilmente entendida en todo el mundo 
             y se adapta de forma perfecta a todo tipo de emprendimientos y negocios.</p>
       </div>
       </div>
@@ -109,15 +104,14 @@ export default function Card (){
       <div className= {s.flipCardFront}>
 
       <ul>
-          <h1> .Website</h1>
+          <h1 class="bg-gradient-to-r from-blue-500 to-transparent"> .Website</h1>
           <h2> Por solo</h2>
           <h3> $1.490</h3>       
       </ul>
       </div>
       <div className={s.flipCardBack}>
-        <h1>Un .website con todas las letras</h1>
-        <p>Encontrar una dirección increible para tu negocio es más fácil que nunca. 
-            Utiliza este nuevo TLD (extensión de dominio de primer nivel)
+        <h1 className={s.h1}>Un .website con todas las letras</h1>
+        <p className={s.p}>Utiliza este nuevo TLD (extensión de dominio de primer nivel)
             y atrapa la atención de potenciales clientes a nivel personal y profesional.</p>
       </div>
       </div>
@@ -135,9 +129,8 @@ export default function Card (){
       </ul>
       </div>
       <div className={s.flipCardBack}>
-        <h1>El preferido a la hora de vender</h1>
-        <p>es otro de los nuevos dominios de mayor éxito en la actualidad.
-        Store significa tienda y de ahí que sea uno de los favoritos 
+        <h1 className={s.h1}>El preferido a la hora de vender</h1>
+        <p className={s.p}>es otro de los nuevos dominios de mayor éxito en la actualidad.
             para crear un ecommerce o proteger la marca del que ya tienes.</p>
       </div>
       </div>
@@ -155,11 +148,10 @@ export default function Card (){
       </ul>
       </div>
       <div className={s.flipCardBack}>
-        <h1>El dominio número .uno para tu web.</h1>
-        <p> te brindará una dirección de Internet corta, fuerte y muy fácil de recordar. 
-            Ideal para destacar nuevos proyectos en la web. 
-            UNO es una palabra de reconocimiento mundial por lo que todas tus 
-            visitas nunca olvidarán tu nombre en Internet.</p>
+        <h1 className={s.h1}>El dominio número .uno para tu web.</h1>
+        <p className={s.p}> Te brindará una dirección de Internet corta, fuerte y muy fácil de recordar. 
+            Ideal para destacar nuevos proyectos en la web.
+            Tus visitas nunca olvidarán tu nombre en Internet.</p>
       </div>
       </div>
       
@@ -176,10 +168,10 @@ export default function Card (){
       </ul>
       </div>
       <div className={s.flipCardBack}>
-        <h1>Promueve tu causa y genera conciencia</h1>
-        <p>Un dominio .org vincula tu organización de forma inmediata con la confianza e integridad
-             de las organizaciones dedicadas al servicio del interés público. Los dominios 
-            .org son unos de los dominios de nivel superior más transparentes del mundo.</p>
+        <h1 className={s.h1}>Promueve tu causa y genera conciencia</h1>
+        <p className={s.p}>Vincula tu organización de forma inmediata con la confianza e integridad
+             de las organizaciones dedicadas al servicio del interés público.
+             </p>
       </div>
       </div>
       
@@ -195,8 +187,8 @@ export default function Card (){
       </ul>
       </div>
       <div className={s.flipCardBack}>
-        <h1>Conviértete en un líder .tech</h1>
-        <p>.TECH es breve, profesional y fácilmente recordable. 
+        <h1 className={s.h1}>Conviértete en un líder .Tech</h1>
+        <p className={s.p}>Es breve, profesional y fácilmente recordable. 
             ¡Es perfecto para atraer
             interesados en adquirir productos tecnológicos 
             y conocer las últimas novedades geeks!</p>
@@ -216,8 +208,8 @@ export default function Card (){
       </ul>
       </div>
       <div className={s.flipCardBack}>
-        <h1>¿a qué esperas para registrar tu dominio .FUN?</h1>
-        <p>Esta extensión es perfecta para la creación de sitios web sobre series, películas, videojuegos, etc.</p>
+        <h1 className={s.h1}>¿a qué esperas para registrar tu dominio .FUN?</h1>
+        <p className={s.p}>Esta extensión es perfecta para la creación de sitios web sobre series, películas, videojuegos, etc.</p>
       </div>
       </div>
       
@@ -235,9 +227,8 @@ export default function Card (){
       </ul>
       </div>
       <div className={s.flipCardBack}>
-        <h1>Quedate en .CASA</h1>
-        <p>El dominio .CASA ha llegado para ocupar en Internet el espacio que le corresponde. 
-            Es una extensión ideal para poner nuestra casa a la venta o crear una página web para una agencia inmobiliaria.
+        <h1 className={s.h1}>Quedate en .CASA</h1>
+        <p className={s.p}>Es una extensión ideal para poner nuestra casa a la venta o crear una página web para una agencia inmobiliaria.
              Registra tu dominio .CASA hoy mismo!</p>
       </div>
       </div>
