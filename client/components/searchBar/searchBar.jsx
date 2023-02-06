ad10c80849f9d7aa795d302a9ebe24df388d5eaf
silvinaarguello <silvinaaguello@gmail.com>
2023-02-05 22:59:07 -0300

    devolviendo lo que se perdio de la ruta dominio-com-co

--- client/components/searchBar/searchBar.jsx
+++ client/components/searchBar/searchBar.jsx
@@ -1,22 +1,52 @@
 import s from "src/styles/searchBar.module.css";
 import Image from "next/image";
 import React, { useState } from 'react'
+import data from "helpers/dataSearchBar";
 
 
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState('')
 
-
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    
+  
+    const results = await data.filter(item => item.includes(searchTerm));
+  
+    console.log(`Search results for "${searchTerm}":`);
+    console.log(results);
+  };
+  
+  
 
   return (
     <form onSubmit={handleSubmit}>
     <div className= {s.containerSearch}>
-
+    <div className={s.inputSearch}>
+    <div className={s.inputResults}>
+ 
+    <input className={s.placeholder} 
+      value={searchTerm}
+      onChange={(event) => setSearchTerm(event.target.value)}
+      placeholder="Ingrese su busqueda" type={'text'} />
+        {/* <div className={s.results}>
+        0 results found
+       </div>  */}
+    
+        </div>
+    <button type="submit" className={s.icon}>
+      
+    <Image 
         src={'/lupa.png'}
         width={40}
         height={40}
         />
-
+              </button>
+     
+            </div>
+         
+          </div>
+          </form>
   )
 }
 export default SearchBar;
