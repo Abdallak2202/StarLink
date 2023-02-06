874c429a969bc81817898070c9dd051432141e87
Abdallak2202 <96998579+Abdallak2202@users.noreply.github.com>
2023-02-06 18:49:20 -0500

    Added Spline dependencie on client/src/pages/index

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react'
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import Spline from '@splinetool/react-spline'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -29,7 +30,7 @@ obtenerServices();
       </Head>
       <main className={styles.main}>
 
-      {/* <Spline className={styles.Spline} scene="https://prod.spline.design/pE91QTxCL5UWQMbK/scene.splinecode" /> */}
+      <Spline className={styles.Spline} scene="https://prod.spline.design/pE91QTxCL5UWQMbK/scene.splinecode" />
 
       </main>
     </>
