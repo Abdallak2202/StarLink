56600a5535fda041af92bdbb18e8fe904a85011f
martinezf1985 <martinezf1985@gmail.com>
2023-02-10 02:16:42 -0300

    cambios en server host

--- client/src/pages/hosting/servidores-dedicados.jsx
+++ client/src/pages/hosting/servidores-dedicados.jsx
@@ -1,6 +1,189 @@
 const servidoresDedicados = () => {
   return (
-    <div>servidores-dedicados</div>
+    
+
+<div>
+
+
+
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+
+
+
+<div>
+
+
+<button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+  <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+      Purple to blue
+  </span>
+</button>
+
+
+</div>
+
+
+      <div>
+
+
+      {/* <h2 className="text-5xl">
+      <span className="text-success">Cloud Servers en America Latina</span>
+    </h2> */}
+    <br/>
+    <h1 className="text-8xl">
+        <span className="text-blue-700 tracking-wide">Administralo o dejalo en manos de nuestros expertos </span>
+    </h1>
+    <br/>
+    <h1 className="text-5xl">
+        <span className="text-blue-700 tracking-wide">Nuestra solución administrada incluye un equipo de expertos que se encargará de cualquier cuestión técnica o pedidos de configuración que necesites realizar. Elige la opción que se adapte a tu negocio.</span>
+    </h1>
+      </div>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+
+
+      <div>
+
+
+<div class="flex flex-col">
+  <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+    <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+      <div class="overflow-hidden">
+        <table class="min-w-full text-center">
+          <thead class="border-b">
+            <tr>
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+                
+              </th>
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+              Auto Administrado
+              </th>
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+              Administrado
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="border-b">
+              <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+               Sitema Operativo
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+               centOS, Ubuntu, Debian, Windows Server
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                Proactivos: Red, Host, Estado de Hardware y Servicios
+              </td>
+            </tr>
+            <tr class="border-b bg-blue-100 border-blue-200">
+              <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+               Parchea y actualizaciones
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+               No
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+              Actualizaciones de seguridad del sistema y panel -
+              Nuevas Funcionalidades Panel Ferozo
+              </td>
+            </tr>
+            <tr class="border-b bg-purple-100 border-purple-200">
+              <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+              Copias de seguridad
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+              A cargo del cliente
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+              Contenido relativo a hosting
+              </td>
+            </tr>
+            <tr class="border-b bg-green-100 border-green-200">
+              <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+              Acceso root
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+              SI
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+              SI
+              </td>
+            </tr>
+            <tr class="border-b bg-red-100 border-red-200">
+              <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+              Panel de control
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+              NO
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+              SI
+              </td>
+            </tr>
+            <tr class="border-b bg-yellow-100 border-yellow-200">
+              <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+                Warning
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+              Migraciones de web hosting 
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+              NO
+              </td>
+            </tr>
+            <tr class="border-b bg-indigo-100 border-indigo-200">
+              <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+              Costo adicional
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+              Instalacion de software bajo demanda
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+              Costo adicional
+              </td>
+            </tr>
+            <tr class="border-b bg-gray-50 border-gray-200">
+              {/* <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+              Costo adicional
+              </td> */}
+              {/* <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                Cell
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                Cell
+              </td> */}
+            </tr>
+            <tr class="border-b bg-gray-800 boder-gray-900">
+              <td class="text-sm text-white font-medium px-6 py-4 whitespace-nowrap">
+                Dark
+              </td>
+              <td class="text-sm text-white font-light px-6 py-4 whitespace-nowrap">
+                Cell
+              </td>
+              <td class="text-sm text-white font-light px-6 py-4 whitespace-nowrap">
+                Cell
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+
+      
+    </div>
   )
 }
 
