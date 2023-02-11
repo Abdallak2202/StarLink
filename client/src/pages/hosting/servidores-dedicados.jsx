6c11cf96c3ce138453ba55f4515f01581cc3ab39
martinezf1985 <martinezf1985@gmail.com>
2023-02-11 11:35:34 -0300

    card en tabla de host-server

--- client/src/pages/hosting/servidores-dedicados.jsx
+++ client/src/pages/hosting/servidores-dedicados.jsx
@@ -12,7 +12,13 @@ const servidoresDedicados = () => {
 <br/>
 <br/>
 
+<div>
 
+<button type="button" class="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2">
+  <svg class="w-4 h-4 mr-2 -ml-1" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="facebook-f" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M279.1 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.4 0 225.4 0c-73.22 0-121.1 44.38-121.1 124.7v70.62H22.89V288h81.39v224h100.2V288z"></path></svg>
+  Sign in with Facebook
+</button>
+</div>
 
 <div>
 
@@ -50,7 +56,8 @@ const servidoresDedicados = () => {
 
 
       <div>
-
+      <a href="#" class="  ml-12 mr-12 block     p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+      
 
 <div class="flex flex-col">
   <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -177,6 +184,7 @@ const servidoresDedicados = () => {
     </div>
   </div>
 </div>
+</a>
 </div>
 
 
