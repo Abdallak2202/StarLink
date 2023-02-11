138051996259a1a704d551f5876c741d0d56d445
martinezf1985 <martinezf1985@gmail.com>
2023-02-11 16:46:55 -0300

    implementacion de card en hostin-dom

--- client/src/pages/hosting/HostingDom.jsx
+++ client/src/pages/hosting/HostingDom.jsx
@@ -6,11 +6,31 @@ import HomeDominio from "../../styles/HomeDominio.module.css";
   return (
 
     <div  className=' '>
-    <div >
+        <br/>
+        <br/>
+
+        <br/>
+
+        <br/>
+
+        <br/>
+
+        <br/>
+
+
+
+      <a href="#" class="  ml-80 mr-80 block     p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+      
+
+
+
+
+        {/* <---------form--------------------------> */}
+    <div  className='bg-violet-500'>
       <br/><br/><br/><br/><br/>
       <form onSubmit='submit'>
 
-      <div class="flex">
+      <div class="flex  ">
     <div class="flex items-center h-5  ">
         <input id="helper-radio" aria-describedby="helper-radio-text" type="radio" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
     </div>
@@ -86,6 +106,7 @@ import HomeDominio from "../../styles/HomeDominio.module.css";
 
 
 </div>
+</a>
 
 </div>
 
