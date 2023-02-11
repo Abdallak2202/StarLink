dc0523ab119deb8e9ab0bcfd6f77264a3e277b3e
luicho76 <luishg.ingeniero@gmail.com>
2023-02-11 08:59:59 -0400

    cambios en CardCloudHosting

--- client/components/Cloud-hosting/CardCloudHosting.jsx
+++ client/components/Cloud-hosting/CardCloudHosting.jsx
@@ -28,7 +28,7 @@ export default function CardCloud () {
     <br/>
     <br/>
     <Link  href='hosting-cloud-servers-vps'>
-  <button type="button" class="text-white text-6xl bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center text-center">Ver Planes</button>
+  <button type="button" class="text-white text-8xl bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center text-center">Ver Planes</button>
 </Link>
         
 </div>
