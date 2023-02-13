5399f8a555c13866b219f8566d7f05452954dc56
silvinaarguello <silvinaaguello@gmail.com>
2023-02-13 04:49:21 -0300

    SearchBar andando pero no filtrando

--- client/src/pages/dominio/dominio-com-co.jsx
+++ client/src/pages/dominio/dominio-com-co.jsx
@@ -1,20 +1,33 @@
 import Card from "components/card-dominio-cpm-co/cardDominios";
 import  SearchBar  from "components/searchBar/searchBar";
+import React from "react";
 
 
 
 
-export const dominioComCo = ({ card }) => {
+export async function getStaticProps() {
+  const req = await fetch("https://star-link-back-end-production.up.railway.app/domains");
+  const cards = await req.json();
+  return { props: { cards } };
+}
+
+
+
+export const dominioComCo = ({ cards, props  }) => {
   return (
     <div>
       <SearchBar />
-      <Card card={card}/>
-    
-    
+
+      {cards.map((cards) => (
+        <Card key={cards.id} TLD={cards.TLD} price = {cards.price} description={cards.description} />
+
+
+      ))}
+
 
     </div>
-    
-  )
-}
+
+  );
+};
 
 export default dominioComCo;
