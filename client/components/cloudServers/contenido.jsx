9a03e5ec83e68ed38d8e0e7a463cfec2b12b6983
EdinsonPerez <perezea1010@gmail.com>
2023-02-17 15:33:27 -0300

    bd conectada a cloudserver

--- client/components/cloudServers/contenido.jsx
+++ client/components/cloudServers/contenido.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
-import data from 'public/utils/data'
+import React, { useEffect, useState, useContext} from 'react'
+// import data from 'public/utils/data'
 import Link from 'next/link'
+import cloudServers from '@/pages/cloudDedicados/cloudServers'
 
-export default function Contenido() {
+
+const Contenido = ({servicio, agregarCarrito}) =>{
+    
+    // const {description, imageSrc, imageAlt, name, category, processing, RAM, STORAGE, transfer, price, id} = servicio[0]
+
+    const handleSubmit = e => {
+        e.preventDefault();
+    }
+    //funcion para agregar al carrito
+    // const servicioSeleccionado = {
+    // id,
+    // imageSrc,
+    // name, 
+    // description,
+    // category,
+    // price,
+    // imageAlt,
+    // processing,
+    // RAM,
+    // STORAGE,
+    // transfer
+    // };
+    
+    // agregarCarrito(servicioSeleccionado);
+
+const [cloud, setCloud] = useState([])
+
+useEffect(() => {
+   // console.log('useEffect')
+    obtenerDatos() 
+}, [])
+const obtenerDatos= async () => {
+    const data = await fetch('https://star-link-back-end-production.up.railway.app/cloud-servers')
+    const services = await data.json()
+    console.log(services)
+    setCloud(services)
+}
+
+
     return (
-      < >
+              < >
       <div className="xl:mx-auto xl:container py-20 2xl:px-0 px-6 ">
           <div className="lg:flex items-center justify-between">
               <div className=" lg:w-1/2 w-full">
@@ -29,50 +68,50 @@ export default function Contenido() {
               <div className="xl:w-1/2s lg:w-7/12 w-full lg:mt-0 mt-12 md:px-8" role="list">
                   <img src="https://i.ibb.co/0n6DSS3/bgimg.png" className="absolute w-full -ml-12 mt-24" alt="background circle images" />
   
-                  {data.products.map((product) => (
+                  {cloud.map((el) => (
                   <div className="pb-4">
                   <div role="listitem" className="bg-white cursor-pointer hover:scale-105 ease-in-out duration-300 drop-shadow-md hover:drop-shadow-xl  rounded-lg p-5 relative z-30 ">
                       <div className="md:flex items-center justify-between">
                       
-                          <h2 id='1' className="text-2xl font-semibold leading-6 text-gray-800">{product.name}</h2>
+                          <h2 className="text-2xl font-semibold leading-6 text-gray-800">{el.name}</h2>
                           
-                          <p className="text-2xl font-semibold md:mt-0 mt-4 leading-6 text-gray-800">${product.price}<span className="font-normal text-base">/mes</span>
+                          <p className="text-2xl font-semibold md:mt-0 mt-4 leading-6 text-gray-800">${el.price}<span className="font-normal text-base">/mes</span>
                           </p>
                           
                       </div>
   
-              <div class="px-6 pt-6 pb-8 bg-white dark:bg-gray-800 sm:p-10 sm:pt-6">      
-              <ul>
-                      <li class="flex items-start mt-4">
-                      <div class="flex-shrink-0">
-                      <svg class="w-6 h-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+              <div className="px-6 pt-6 pb-8 bg-white dark:bg-gray-800 sm:p-10 sm:pt-6">      
+              <ul onSubmit={handleSubmit}>
+                      <li className="flex items-start mt-4">
+                      <div className="flex-shrink-0">
+                      <svg className="w-6 h-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
                   </svg>
                   </div>
-                  <p class="ml-3 text-base leading-6 text-gray-700 dark:text-gray-200">
-                  Memoria: {product.RAM}
+                  <p className="ml-3 text-base leading-6 text-gray-700 dark:text-gray-200">
+                  Memoria: {el.RAM}
                   </p> 
                   </li>
   
-                  <li class="flex items-start mt-4">
-                      <div class="flex-shrink-0">
-                      <svg class="w-6 h-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                  <li className="flex items-start mt-4">
+                      <div className="flex-shrink-0">
+                      <svg className="w-6 h-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
                   </svg>
                   </div>
                   <p class="ml-3 text-base leading-6 text-gray-700 dark:text-gray-200">
-                  Storage: {product.STORAGE}
+                  Storage: {el.SSD}
                   </p> 
                   </li>
   
-                  <li class="flex items-start mt-4">
-                      <div class="flex-shrink-0">
-                      <svg class="w-6 h-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                  <li className="flex items-start mt-4">
+                      <div className="flex-shrink-0">
+                      <svg className="w-6 h-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
                   </svg>
                   </div>
-                  <p class="ml-3 text-base leading-6 text-gray-700 dark:text-gray-200">
-                  Transferencia: {product.transfer}
+                  <p className="ml-3 text-base leading-6 text-gray-700 dark:text-gray-200">
+                  Transferencia: {el.transfer}
                   </p> 
                   </li>
               </ul>
@@ -87,10 +126,12 @@ export default function Contenido() {
                   </div>
                   </div>
                 
-                  ))}
+                  ))} 
               </div>
           </div>
       </div>
   </>
     )
   }
+
+  export default Contenido;
