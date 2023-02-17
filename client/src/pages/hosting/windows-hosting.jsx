34e72fca26c3e766e3aa38408ce435685ea89106
luicho76 <luishg.ingeniero@gmail.com>
2023-02-16 21:57:19 -0400

    refactorizando pasarela de pagos

--- client/src/pages/hosting/windows-hosting.jsx
+++ client/src/pages/hosting/windows-hosting.jsx
@@ -1,22 +1,8 @@
-/* const windowsHosting = () => {
+const windowsHosting = () => {
   return (
     <div>windows-hosting</div>
   )
 }
 
-export default windowsHosting; */
-
-import CardPagos from "../../../components/pasarelaPago/PasarelaP";
-
-
-export const Pagos = () => {
-  return (
-    <div>
-      <CardPagos />
-    </div>
-    
-  )
-}
-
-export default Pagos;
+export default windowsHosting;
 
