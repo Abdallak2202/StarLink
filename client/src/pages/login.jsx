882acf75c36f6c60ce5f4bc088c0d5559c9dff7c
silvinaarguello <silvinaaguello@gmail.com>
2023-02-24 03:54:22 -0300

    acomodando estilos de login

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -156,7 +156,7 @@ const Login = (
         <div className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
           <div className="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0">
             <img
-              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+              src="images/loginConLogoStarLink.png"
               class="w-full"
               alt="Sample image"
             />
@@ -164,7 +164,7 @@ const Login = (
           <div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
             <form onSubmit={handleSubmit}>
               <div class="flex flex-row items-center justify-center lg:justify-start">
-                <p class="text-lg mb-0 mr-4">Sign in with</p>
+                <p class="text-3xl mb-0 mr-4">Sign in with</p>
                 <button
                   type="button"
                   data-mdb-ripple="true"
@@ -175,7 +175,7 @@ const Login = (
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 320 512"
-                    class="w-4 h-4"
+                    class="w-8 h-8"
                   >
                     {/* <!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --> */}
                     <path
@@ -199,7 +199,7 @@ const Login = (
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 488 512"
-                    class="w-4 h-4"
+                    class="w-8 h-8"
                   >
                     {/* <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->*/}
 
@@ -222,7 +222,7 @@ const Login = (
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 448 512"
-                    class="w-4 h-4"
+                    class="w-8 h-8"
                   >
                     {/* <!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --> */}
                     <path
@@ -234,14 +234,14 @@ const Login = (
               </div>
 
               <div className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
-                <p className="text-center font-semibold mx-4 mb-0">Or</p>
+                <p className="text-center font-semibold mx-4 mb-0 text-xl">Or</p>
               </div>
 
               {/* <!-- Email input --> */}
               <div class="mb-6">
                 <input
                   type="text"
-                  class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  class="form-control block w-full px-4 py-2 text-3xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                   id="exampleFormControlInput2"
                   placeholder="Email address"
                   name="identifier"
@@ -254,7 +254,7 @@ const Login = (
               <div className="mb-2">
                 <input
                   type="password"
-                  class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  class="form-control block w-full px-4 py-2 text-3xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                   id="exampleFormControlInput2"
                   placeholder="Password"
                   name="password"
@@ -278,26 +278,26 @@ const Login = (
                     id="exampleCheck2"
                   />
                   <label
-                    className="form-check-label inline-block text-gray-800"
+                    className="form-check-label inline-block text-gray-800 text-2xl"
                     for="exampleCheck2"
                   >
                     Remember me
                   </label>
                 </div>
-                <a href="#!" class="text-gray-800">
+                <a href="#!" class="text-gray-800 text-2xl">
                   Forgot password?
                 </a>
               </div>
 
               <div className="text-center lg:text-left">
-                <button className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+                <button className="inline-block px-8 py-3 bg-blue-600 text-white font-medium text-2xl leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
                   {isLoading ? "Loading..." : "Sign In"}
                 </button>
                 <p className="text-sm font-semibold mt-2 pt-1 mb-0">
                   Don't have an account?
                   <Link
                     href="/register"
-                    className="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
+                    className= "text-xl text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
                   >
                     Register
                   </Link>
