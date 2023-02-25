91125348691a4d4b41925fbe1c5dd5bc190a168f
silvinaarguello <silvinaaguello@gmail.com>
2023-02-25 19:40:22 -0300

    Estilos terminados de hosting

--- client/components/card-hosting/CardHosting.jsx
+++ client/components/card-hosting/CardHosting.jsx
@@ -10,17 +10,25 @@ const config = {
       damping: 10,
       stiffness: 80
     };
-    export const CardHosting = ({ price, description, name, floorSpace, racks, consumption , id , slug }) => {
+    export const CardHosting = ({ price, description, name, floorspace, racks, consumption , id , slug }) => {
       return (
 
-<div>
-
-<div>
-    
-</div>
 
 
-<div >
+        <div className={s.container}>
+
+<div class={s.hero}> 
+
+  <div class={s.cube}></div>
+  <div class={s.cube}></div>
+  <div class={s.cube}></div>
+  <div class={s.cube}></div>
+  <div class={s.cube}></div>
+  <div class={s.cube}></div>
+
+
+
+
 
         <motion.div
           transition={config}
@@ -28,62 +36,55 @@ const config = {
            animate={{ scale: 1, opacity: 1, transitionDuration: '0.2s' }}
             exit={{ x: 0, opacity: 0 }}>
 
-        <div className={s.container}>
-        <div className= {s.flipCard} >
-              <div className={s.flipCardInner}>
-             <div className= {s.flipCardFront} >
+<div className={s.flipCardFront}>
+<div class="  w-full max-w-sm p-6 bg-white border-8 border-gray-200 rounded-lg shadow sm:p-12 dark:bg-gray-800 dark:border-gray-700">
 
-             <h1 className="mb-4 text-9xl font-medium font-sans md:font-serif bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-cyan-700 to-blue-600  ">{slug}</h1>
-             <h1 className="mb-4 text-xl font-medium text-purple-500 dark:text-gray-400">{name}</h1>
-             <h2 className="mb-4 text-xl font-medium text-purple-500 dark:text-gray-400"> {floorSpace}</h2>
-             <h2 className="mb-4 text-xl font-medium text-purple-500 dark:text-gray-400"> {racks}</h2>
-             <h2 className="mb-4 text-xl font-medium text-purple-500 dark:text-gray-400"> {consumption}</h2>
+<h1 className="text-white ring-offset-2 ring-2 shadow-inner bg-gradient-to-r from-indigo-500 to-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 rounded-lg font-bold font-mono text-4xl px-4 py-2 inline-flex justify-center w-full text-center">{name}</h1>
+             <h1 className="italic mb-2 px-3 py-4 text-2xl font-mono font-medium text-gray-500 dark:text-gray-400 ">Elige tu Plan</h1>
+             <p className=" mb-6 text-4xl font-bold text-blue-400 drop-shadow-lg shadow-ambar-300 text-center">{description}</p>
+              {/* <!-- List --> */}
+              <ul role="list" class="space-y-5 my-7">
+              <li class="flex space-x-3">
+              {/* <!-- Icon --> */}
+              <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-blue-600 dark:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+              <span class="text-2xl font-bold leading-tight text-gray-500 dark:text-gray-400">{floorspace} IN</span>
+              </li>
+              <li class="flex space-x-3">
+              {/* <!-- Icon --> */}
+              <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-blue-600 dark:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+              <span class="text-2xl font-bold leading-tight text-gray-500 dark:text-gray-400"> {racks} unidad de racks</span>
+              </li>
+              <li class="flex space-x-3">
+              {/* <!-- Icon --> */}
+              <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-blue-600 dark:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+              <span class="text-2xl font-bold leading-tight text-gray-500 dark:text-gray-400"> {consumption} kWh</span>
+              </li>
+  
 
+</ul>
 
-
-          <h2 className="mb-4 text-xl font-medium text-purple-500 dark:text-gray-400"> Por solo</h2>
-          <div className="flex items-baseline text-purple-500 dark:text-white">
-          <span class="text-5xl font-semibold ">$</span>
-          <span class="text-7xl font-extrabold tracking-tight ">{price}</span>
-          <span class="ml-1 text-2xl font-normal text-purple-500 dark:text-gray-400">/month</span>
-
+          <h2 className=" underline decoration-wavy italic mb-4 px-3 py-4 text-2xl font-mono font-medium text-gray-500 dark:text-gray-400"> Por solo</h2>
+          <div className="flex items-baseline text-purple-500 dark:text-white drop-shadow-2xl">
+          <span class="text-4xl font-semibold">$</span>
+          <span class="text-7xl py-8 font-extrabold tracking-tight">{price}</span>
+          <span class="ml-1 text-2xl font-normal text-gray-500 dark:text-gray-400">/month</span>
           </div>
-          </div>
-
-          <div className={s.flipCardBack}>
-            <br/>
-          <h1 className="mb-4 text-6xl font-extrabold text-purple-200  drop-shadow-lg">Eleji tu Plan {name}</h1>
-          <br/>
-          <br/>
-          <p className=" mb-6 text-4xl font-bold text-purple-200 drop-shadow-lg shadow-ambar-300">{description}</p>
-          
-          <br/>
-          
-
-          {/* <Link href='HostingDom'>
-
-
-<button disabled type="button" class="py-2.5 px-5 mr-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-purple-200 hover:bg-purple-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 inline-flex items-center">
-    <svg aria-hidden="true" role="status" class="inline w-4 h-4 mr-3 text-gray-200 animate-spin dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="#1C64F2"/>
-    </svg>
-    continuar
-</button>
-</Link> */}
                       <Link href={`/racks/${id}`} >
-                        <button className="bg-gray-100 focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:outline-none text-base leading-none text-gray-600 rounded-full py-3 px-10 mr-1" id="monthly">
+                        <button           class="cursor-pointer text-gray-100 text-2xl font-semibold transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300  rounded-lg  px-6 py-4 inline-flex justify-center w-full text-center">
+
                              View Product
                         </button>
+           
                       </Link>  
 
         </div>
+
         </div>
-          </div>
-          </div>
+     
           </motion.div>
           </div>
           </div>
+         
       );
     };
 
