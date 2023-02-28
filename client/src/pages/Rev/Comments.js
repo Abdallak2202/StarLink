90b9f93af753a6da6fad55868452e4e56c46cc0b
martinezf1985 <martinezf1985@gmail.com>
2023-02-28 02:57:00 -0300

    creacion de rama review

---
+++ client/src/pages/Rev/Comments.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import CommentsForm from 'components/rev-comments/CommentsForm'
+import CommentsCard from 'components/rev-comments/CommentsCard'
+
+const Comments = () => {
+  return (
+    <div>
+<CommentsCard/>
+
+<CommentsForm/>
+
+
+    </div>
+  )
+}
+
+export default Comments
