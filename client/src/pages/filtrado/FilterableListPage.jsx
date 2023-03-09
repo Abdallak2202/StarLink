f961e6e0f01366fc1dc868db607f35da68699a44
EdinsonPerez <perezea1010@gmail.com>
2023-03-09 14:39:18 -0300

    mensaje en el filtro

--- client/src/pages/filtrado/FilterableListPage.jsx
+++ client/src/pages/filtrado/FilterableListPage.jsx
@@ -46,64 +46,60 @@ export default function FilterableList() {
   const handlerOnChange = ({name, value}) => setSelectedOptions(prevSelectedOptions => ({...prevSelectedOptions, [name.split("-")[0]]: value}))
 
   return (
-    
-        <div>
-      <Filter data={data} onChange={handlerOnChange} />
-     
-     
+  <div>
+    <Filter data={data} onChange={handlerOnChange} />
 
-     {filteredData.map((item, index) => (
+    {filteredData.length === 0 && (
+      <p class="text-5xl px-20 underline underline-offset-3 decoration-8 decoration-blue-400 dark:decoration-blue-600">No hay resultados en esa búsqueda</p>
+    )}
+
+    {filteredData.length > 0 &&
+      filteredData.map((item, index) => (
         <div key={index}>
-  <div class=" pt-40 relative overflow-x-auto shadow-md sm:rounded-lg">
- <div class="flex items-center justify-between pb-4">
-
- <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-      <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-              <th scope="col" class="px-6 py-3 text-center">
-                   Servicio
-                </th>
-               <th scope="col" class="px-6 py-3 text-center">
-                   Precio
-               </th>
-                <th scope="col" class="px-6 py-3 text-center">
-                   Descripción
-                </th>
-                 <th scope="col" class="px-6 py-3 text-center">
-                  Categoria
-             </th>
-           </tr>
-       </thead>
-        <tbody>
-        <tr class=" text-lg text-center leading-relaxed bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th scope="row" class="px-6 py-4 text-center sfont-medium text-gray-900 whitespace-nowrap dark:text-white">
-               {item.slug}
-                 </th>
-                 <td class="px-8 py-20 text-center">
-                ${item.price}
-                </td>
-                <td class="px-8 py-20 text-center">
-                {item.description}
-                </td>
-                <td class="px-8 py-20 text-center ">
-                {item.service}
-                 </td>
-
-             </tr>
-        </tbody>
-    </table>
-</div>
-
+          <div class="pt-40 relative overflow-x-auto shadow-md sm:rounded-lg">
+            <div class="flex items-center justify-between pb-4">
+              <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                  <tr>
+                    <th scope="col" class="px-6 py-3 text-center">
+                      Servicio
+                    </th>
+                    <th scope="col" class="px-6 py-3 text-center">
+                      Precio
+                    </th>
+                    <th scope="col" class="px-6 py-3 text-center">
+                      Descripción
+                    </th>
+                    <th scope="col" class="px-6 py-3 text-center">
+                      Categoría
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr class="text-lg text-center leading-relaxed bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <th scope="row" class="px-6 py-4 text-center sfont-medium text-gray-900 whitespace-nowrap dark:text-white">
+                      {item.slug}
+                    </th>
+                    <td class="px-8 py-20 text-center">
+                      ${item.price}
+                    </td>
+                    <td class="px-8 py-20 text-center">
+                      {item.description}
+                    </td>
+                    <td class="px-8 py-20 text-center ">
+                      {item.service}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
-      
-       
-</div>
-
-          
-        
-       ))}
-    
+      ))
+    }
+    {/* {filteredData.length === 0 && (
+      <p>No se encontró ningún servicio para esta búsqueda</p>
+    )} */}
   </div>
-  );
-
- }
+);
+    }
